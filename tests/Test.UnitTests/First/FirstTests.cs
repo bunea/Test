efc009e4e82146975390c0201a656089dd8692dc
bunea <andrei@bunea.me>
2018-05-19 13:42:03 +0300

    add test data with odd numbers

--- tests/Test.UnitTests/First/FirstTests.cs
+++ tests/Test.UnitTests/First/FirstTests.cs
@@ -8,6 +8,7 @@ namespace Test.UnitTests.First
     {
         [Theory]
         [InlineData(new int[] { 1, 2, 3, 4, 5, 6 }, 12)]
+        [InlineData(new int[] { 1, 3, 5, 7, 9, 11 }, 0)]
         public void ShouldSumEvens(int[] arr, int expected)
         {
             var sumEvens = new SumEvens();
