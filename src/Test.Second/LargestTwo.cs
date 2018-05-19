ef84049cb1fa418dae010aa3f85f77e36df31dc8
bunea <andrei@bunea.me>
2018-05-19 19:11:36 +0300

    implement Largest. tests pass

--- src/Test.Second/LargestTwo.cs
+++ src/Test.Second/LargestTwo.cs
@@ -6,7 +6,27 @@ namespace Test.Second
     {
         public int[] Largest(int[] arr)
         {
-            throw new NotImplementedException();
+            if (arr.Length < 2)
+            {
+                throw new ArgumentException("Array should have at least 2 elements");
+            }
+
+            var first = int.MinValue;
+            var second = int.MinValue;
+            foreach (var num in arr)
+            {
+                if (first < num)
+                {
+                    second = first;
+                    first = num;
+                }
+                else if (second < num)
+                {
+                    second = num;
+                }
+            }
+
+            return new int[] { first, second };
         }
     }
 }
